df2d76d3a38f94ef36b1b968d5e60652483b3253
Sun Cheng <406504325@qq.com>
2024-04-22 15:48:03 +0800

    uni vue components reference contributor

--- src/main/kotlin/com/nevermore/uniapphelper/psi/EasyComReferenceContributor.kt
+++ src/main/kotlin/com/nevermore/uniapphelper/psi/EasyComReferenceContributor.kt
@@ -2,7 +2,8 @@ package com.nevermore.uniapphelper.psi
 
 import com.intellij.lang.javascript.psi.JSImplicitElementProvider
 import com.intellij.openapi.util.TextRange
-import com.intellij.patterns.PlatformPatterns
+import com.intellij.patterns.StandardPatterns.string
+import com.intellij.patterns.XmlPatterns
 import com.intellij.psi.*
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.stubs.StubIndex
@@ -16,24 +17,20 @@ import org.jetbrains.vuejs.index.VUE_COMPONENTS_INDEX_KEY
  * @since
  */
 class EasyComReferenceContributor : PsiReferenceContributor() {
+
     override fun registerReferenceProviders(registrar: PsiReferenceRegistrar) {
         registrar.registerReferenceProvider(
-            PlatformPatterns.psiElement(XmlTag::class.java),
+            XmlPatterns.xmlTag().withName(string().startsWith("uni").andOr(string().startsWith("Uni"))),
             object : PsiReferenceProvider() {
                 override fun getReferencesByElement(
                     element: PsiElement, context: ProcessingContext
                 ): Array<PsiReference> {
                     val value = element.asXmlTagToGetTagName()
-
-                    if (value.startsWith("uni")) {
-                        return arrayOf(EasyComReference(element, TextRange(0, value.length)))
-                    }
-                    return PsiReference.EMPTY_ARRAY
+                    return arrayOf(EasyComReference(element, TextRange(0, value.length)))
                 }
 
             })
     }
-
 }
 
 class EasyComReference(element: PsiElement, textRange: TextRange) : PsiReferenceBase<PsiElement>(element, textRange) {
@@ -53,7 +50,6 @@ class EasyComReference(element: PsiElement, textRange: TextRange) : PsiReference
             })
         return result.firstOrNull()?.element
     }
-
 }
 
 private fun PsiElement.asXmlTagToGetTagName() = (this as XmlTag).name
